6f0ef2afbd0b5de08515744d92b2c2fb01f7e4b3
Ricardo <ricardoalbornozgil@live.com>
2024-09-02 23:55:40 -0400

    Render conf

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,17 @@
 "use strict";
-/** Database setup for jobly. */
+/** Database setup for Jobly. */
 
 const { createClient } = require("@supabase/supabase-js");
 const { getDatabaseUri, SUPABASE_URL, SUPABASE_ANON_KEY } = require("./config");
+const { Client } = require("pg");
 
 let db;
 
 if (process.env.NODE_ENV === "production") {
+  // Connect to Supabase in production
   db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 } else {
-  const { Client } = require("pg");
+  // Connect to PostgreSQL locally for development and testing
   db = new Client({
     connectionString: getDatabaseUri(),
   });
