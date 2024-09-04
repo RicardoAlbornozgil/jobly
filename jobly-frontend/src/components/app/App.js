dab60b932949ff25decd6e4820fc5adbb5215bdb
Ricardo <ricardoalbornozgil@live.com>
2024-09-03 22:10:10 -0400

    Modified app.js

--- jobly-frontend/src/components/app/App.js
+++ jobly-frontend/src/components/app/App.js
@@ -40,20 +40,15 @@ function App() {
     "token=", token,
   );
 
-  // Load user info from API. Until a user is logged in and they have a token,
-  // this should not run. It only needs to re-run when a user logs out, so
-  // the value of the token is a dependency for this effect.
-
   useEffect(function loadUserInfo() {
     console.debug("App useEffect loadUserInfo", "token=", token);
 
     async function getCurrentUser() {
-      if (token) {
+      if (token && token.success !== false) {
         try {
           const decodedToken = jwt.decode(token);
           if (decodedToken) {
             let { username } = decodedToken;
-            // Put the token on the Api class so it can use it to call the API.
             JoblyApi.token = token;
             let currentUser = await JoblyApi.getCurrentUser(username);
             setCurrentUser(currentUser);
@@ -66,29 +61,22 @@ function App() {
           console.error("App loadUserInfo: problem loading", err);
           setCurrentUser(null);
         }
+      } else {
+        console.error("Invalid or missing token.");
+        setCurrentUser(null);
       }
       setInfoLoaded(true);
     }
 
-    // Set infoLoaded to false while async getCurrentUser runs; once the
-    // data is fetched (or even if an error happens!), this will be set back
-    // to true to control the spinner.
     setInfoLoaded(false);
     getCurrentUser();
   }, [token]);
 
-  /** Handles site-wide logout. */
   function logout() {
     setCurrentUser(null);
     setToken(null);
   }
 
-  /** Handles site-wide signup.
-   *
-   * Automatically logs them in (set token) upon signup.
-   *
-   * Make sure you await this function and check its return value!
-   */
   async function signup(signupData) {
     try {
       let token = await JoblyApi.signup(signupData);
@@ -100,10 +88,6 @@ function App() {
     }
   }
 
-  /** Handles site-wide login.
-   *
-   * Make sure you await this function and check its return value!
-   */
   async function login(loginData) {
     try {
       let token = await JoblyApi.login(loginData);
@@ -115,12 +99,10 @@ function App() {
     }
   }
 
-  /** Checks if a job has been applied for. */
   function hasAppliedToJob(id) {
     return applicationIds.has(id);
   }
 
-  /** Apply to a job: make API call and update set of application IDs. */
   function applyToJob(id) {
     if (hasAppliedToJob(id)) return;
     JoblyApi.applyToJob(currentUser.username, id);
