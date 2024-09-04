59efb197265e10f07853b3e9fd54ec8e9a5ba027
Ricardo <ricardoalbornozgil@live.com>
2024-09-03 23:20:48 -0400

    Modified

--- jobly-frontend/src/hooks/useLocalStorage.js
+++ jobly-frontend/src/hooks/useLocalStorage.js
@@ -2,21 +2,23 @@ import { useState, useEffect } from "react";
 
 /** Custom hook for keeping state data synced with localStorage.
  *
- * This creates `item` as state and look in localStorage for current value
- * (if not found, defaults to `firstValue`)
+ * This creates `item` as state and looks in localStorage for current value
+ * (if not found, defaults to `firstValue`).
  *
  * When `item` changes, effect re-runs:
  * - if new state is null, removes from localStorage
  * - else, updates localStorage
  *
  * To the component, this just acts like state that is also synced to/from
- * localStorage::
+ * localStorage.
  *
  *   const [myThing, setMyThing] = useLocalStorage("myThing")
  */
 
 function useLocalStorage(key, firstValue = null) {
-  const initialValue = localStorage.getItem(key) || firstValue;
+  const initialValue = localStorage.getItem(key) 
+    ? JSON.parse(localStorage.getItem(key)) 
+    : firstValue;
 
   const [item, setItem] = useState(initialValue);
 
@@ -26,7 +28,7 @@ function useLocalStorage(key, firstValue = null) {
     if (item === null) {
       localStorage.removeItem(key);
     } else {
-      localStorage.setItem(key, item);
+      localStorage.setItem(key, JSON.stringify(item));
     }
   }, [key, item]);
 
