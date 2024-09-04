eb978431162c01be95acc9064155bce7e0172e17
Ricardo <ricardoalbornozgil@live.com>
2024-09-03 22:33:57 -0400

    Modified UserContext.js

--- jobly-frontend/src/components/auth/UserContext.js
+++ jobly-frontend/src/components/auth/UserContext.js
@@ -1,23 +1,7 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 
-/** Context: provides currentUser object and setter for it throughout the app. */
+/** Context: provides currentUser object and setter for it throughout app. */
 
-const UserContext = React.createContext(null);
-
-/** Hook to access the UserContext */
-export const useUser = () => {
-  return useContext(UserContext);
-};
-
-/** UserProvider component to wrap around components needing access to UserContext */
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  return (
-    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+const UserContext = React.createContext();
 
 export default UserContext;
